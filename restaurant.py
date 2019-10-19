19617bd8c6d5b0b62e5f0ba1f75ad439f1b6fa9f
Donald Hacker <donaldhacker@tamu.edu>
2019-10-19 15:38:27 -0500

    Classes Finished

--- restaurant.py
+++ restaurant.py
@@ -2,5 +2,22 @@
 #This is a restaurant object
 
 class Restaurant:
-    def __init__(self,id,address,category,city,country,cuisine,dateAdded,dateUpdated, key,
-                 latitude, longitude,menuUrl,):
+    def __init__(self,id,category,country,province,city,address,postalcode,latitude,longitude,website,menuUrl)
+        self.id  = id
+        self.category = category
+        self.country = country
+        self.province = province
+        self.city = city
+        self.address = address
+        self.postalcode = postalcode
+        self.latitude = latitude
+        self.longitude  = longitude
+        self.website = website
+        slef.menuUrl = menuUrl
+
+class Food:
+    def __init__(self,id,menuname,menudescription,cost)
+        self.id = id
+        self.menuname = menuname
+        self.menudescription = menudescription
+        self.cost = cost
