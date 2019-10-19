15eb073db4188f4b42d3c058a0221f46eec6b6fe
danbeltran13 <danielbeltran1303@gmail.com>
2019-10-19 14:44:51 -0500

    UPLOAD1

---
+++ csv_reader.py
@@ -0,0 +1,17 @@
+
+
+# Names of Files
+csv_file = "data.csv"
+
+# opens files
+data = open(csv_file,"r")
+
+# Reads the first line of the csv file
+properties = data.readline().rstrip("\n").split(',')
+
+# Loops through the csv file line by line
+for line in data:
+    # turns the entire line into an array
+    line = line.rstrip('\n').split(',')
+    print(line)
+print(properties)
---
+++ restaurant.py
@@ -0,0 +1,6 @@
+
+#This is a restaurant object
+
+class Restaurant:
+    def __init__(self,id,address,category,city,country,cuisine,dateAdded,dateUpdated, key,
+                 latitude, longitude,menuUrl,):
