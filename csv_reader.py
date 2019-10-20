8b4b709b9a053670a6a869109471f499cacf174c
Donald Hacker <donaldhacker@tamu.edu>
2019-10-20 00:05:43 -0500

    added data.next()

--- csv_reader.py
+++ csv_reader.py
@@ -1,4 +1,7 @@
 import csv
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 # Class Declarations
 class Restaurant(object):
@@ -27,18 +30,25 @@ class Food(object):
 
 
 
+# cost of tacos and burritos based on zip codes and city
 
 
 # Names of Files
 csv_file = open("just tacos and burritos.csv")
 data = csv.reader(csv_file)
+data.next();
+
+
+
+
+#test and correct Data
 
 
 restaurantList = []
 hasMenu = True
 previousId = "first"
 
-# Loops through the csv file line by line
+# (Restaurant Data)
 for line in data:
 
     # checks to see if there is a menu on the website
@@ -47,27 +57,61 @@ for line in data:
     else:
         hasMenu = "False"
 
+    id=line[0]
+    name=line[11]
+    category=line[2]
+    province=line[13]
+    city=line[3]
+    address=line[1]
+    postalcode=line[12]
+    latitude=line[5]
+    longitude=line[6]
+    website=line[14]
+    menuname = line[10]
+    menudescription = line[9]
+    cost = line[8]
+
     # checks to see if the same restaruant is repeated,
     # if it is, then append the menu o include a new food item
     if previousId != line[0]:
-        restaurantList.append(Restaurant(line[0],line[11],line[2],line[13],line[3],line[1],line[12],line[5],line[6],line[14],hasMenu))
-        restaurantList[len(restaurantList)-1].menu.append(Food(line[10],line[9],line[8]))
+        # places an NULL for empty values
+        restaurantList.append(Restaurant(id,name,category,province,city,address,postalcode,latitude,longitude,website,hasMenu))
+        restaurantList[len(restaurantList)-1].menu.append(Food(menuname,menudescription,cost))
         previousId = line[0]
     else:
-        restaurantList[len(restaurantList)-1].menu.append(Food(line[10],line[9],line[8]))
+        restaurantList[len(restaurantList)-1].menu.append(Food(menuname,menudescription,cost))
+
+# (Yelp Data)
+#
+
+
+
+#raw data calculations
+
+# mean of tacos and burrtios
+# for x in restaurantList:
+#     # remove empty data and null
+#     costTotal = 0
+#     count = 0
+#     menuSize = 0
+
+# for x in restaurantList:
+#     print len(x.menu), "\n"
+
 
 
 
 #How to access The data
 # Note: Do not index 0, its the name of the columns
-print "id: " + restaurantList[1].id + "\n"
-print "name: " + restaurantList[1].name + "\n"
-print "city: " + restaurantList[1].city + "\n"
-print "address: " + restaurantList[1].address + "\n"
-print "postalcode: " + restaurantList[1].postalcode + "\n"
-print "website: " + restaurantList[1].website + "\n"
-print "hasmenuUrl: " + restaurantList[1].menuUrl + "\n"
-
-print "menu name: " + restaurantList[1].menu[1].menuname + "\n"
-print "menudescription: " + restaurantList[1].menu[1].menudescription + "\n"
-print "(this item does not have a cost)" + restaurantList[1].menu[1].cost + "\n"
+# print "id: " + restaurantList[1].id + "\n"
+# print "name: " + restaurantList[1].name + "\n"
+# print "city: " + restaurantList[1].city + "\n"
+# print "province: " + restaurantList[1].province + "\n"
+# print "address: " + restaurantList[1].address + "\n"
+# print "postalcode: " + restaurantList[1].postalcode + "\n"
+# print "website: " + restaurantList[1].website + "\n"
+# print "hasmenuUrl: " + restaurantList[1].menuUrl + "\n"
+#
+# print "menu name: " + restaurantList[1].menu[1].menuname + "\n"
+# print "menudescription: " + restaurantList[1].menu[1].menudescription + "\n"
+# print "cost" + restaurantList[1].menu[1].cost + "\n"
